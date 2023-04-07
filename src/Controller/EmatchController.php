67dcd15cfb1d9816bb17235af2b16cbc9d0e55c3
kevinvanphavong <kvanphavong@webqam.fr>
2023-04-07 09:22:27 +0200

    create score form for the team/players

--- src/Controller/EmatchController.php
+++ src/Controller/EmatchController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Ematch;
 use App\Entity\Game;
+use App\Form\ScoreType;
 use App\Repository\EmatchRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,10 +27,25 @@ class EmatchController extends AbstractController
     #[Route('/game/{game_id}/ematch/{ematch_id}', name: 'ematch_edit', methods: ['get'])]
     #[ParamConverter('game', class: 'App\Entity\Game', options: ['id' => 'game_id'])]
     #[ParamConverter('ematch', class: 'App\Entity\Ematch', options: ['id' => 'ematch_id'])]
-    public function editEmatchScore(Game $game, Ematch $ematch, EmatchRepository $ematchRepository): Response
+    public function editEmatchScore(Game $game, Ematch $ematch, EmatchRepository $ematchRepository, Request $request): Response
     {
-        return $this->render('ematch/edit.html.twig', [
+        $scoreForm = $this->createForm(ScoreType::class);
+        $scoreForm->handleRequest($request);
+
+        if ($scoreForm->isSubmitted() && $scoreForm->isValid()) {
+
+            dump($scoreForm->getData());
+            die();
+
+//            return $this->redirectToRoute('ematches', [], Response::HTTP_SEE_OTHER);
+        }
+
+
+        return $this->render('ematch/edit-score.html.twig', [
             'game' => $game,
+            'ematch' => $ematch,
+            'teams' => $ematch->getTeams(),
+            'scoreForm' => $scoreForm->createView(),
             'ematches' => $ematchRepository->findBy(['game' => $game->getId()]),
         ]);
     }
---
+++ src/Form/ScoreType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Score;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ScoreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('totallPoints')
+            ->add('numberKills')
+            ->add('numberDeaths')
+            ->add('numberAssists')
+//            ->add('player')
+//            ->add('ematch')
+//            ->add('team')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Score::class,
+        ]);
+    }
+}
